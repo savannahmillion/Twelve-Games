46b97c18cca6d90fe78fe41a5ac423835171e8d4
Savannah <savannahmillion@gmail.com>
2014-11-06 13:17:12 -0600

    added php includes for header and footer

---
+++ index.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+	<?php include("includes/head.html");?>
+</head>
+
+<body>
+
+	<?php include("includes/header.html");?>
+	<h2>this is a test</h2>
+	<p>this is some content</p>
+	<?php include("includes/footer.html");?>
+
+</body>
+
+</html>
