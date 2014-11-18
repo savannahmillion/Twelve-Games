bef5223e00b25efbe00f2e09bac4adb2764610c4
Savannah <savannahmillion@gmail.com>
2014-11-18 15:08:17 -0600

    finessed about page

--- about.php
+++ about.php
@@ -11,7 +11,33 @@
 
 		<div class="main-content center">
 			<h1>about</h1>
-			<p>this is some content</p>
+
+			<div class="about-content">
+				<p>
+					Twelve Games is a set of twelve micro-games released over twelve days preceding Christmas. David and Savannah initiated it as a fun exploration in HTML5 game frameworks and learning GitHub together. The project is hosted <a href="https://github.com/savannahmillion/Twelve-Games">here on GitHub</a> if you'd like to check it out! The games were built with <a href="http://phaser.io/">phaser.io</a>.
+				</p>
+				<div class="bio-content">
+					<img src="img/david-avatar.png" class="avatar">
+					<h2>
+						<a href="http://david-laskey.com/">david laskey</a>
+					</h2>
+					<p>
+						David is an independent game developer in Chicago. He is a cofounder of <a href="http://theamiable.com/">The Amiable</a>, currently working on their premier cooperative action game, <a href="http://tetrapulse.com/">Tetrapulse</a>. He did all the programmer-y things for Twelve Games.
+					</p>
+					<a class="img-link" href="https://twitter.com/david_laskey"><img class="twitter" src="img/twitter.png"></a>
+				</div>
+				<div class="bio-content">
+					<img src="img/savannah-avatar.png" class="avatar">
+					<h2>
+						<a href="http://sava.nnah.rocks/">savannah million</a>
+					</h2>
+					<p>
+						Savannah is an interaction designer in Chicago. She is currently re-designing and building internet for <a href="http://kenshoo.com/">Kenshoo</a>, a marketing technology company. She did all the art and web-y things for Twelve Games.
+					</p>
+					<a class="img-link" href="https://twitter.com/savannahmillion"><img class="twitter" src="img/twitter.png"></a>
+				</div>
+			</div>
+
 		</div>
 
 	<?php include("includes/footer.html");?>
