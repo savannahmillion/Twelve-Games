894099be6713d1132645eecc718383be35619b37
Savannah <savannahmillion@gmail.com>
2014-11-17 18:07:38 -0600

    re-did layout again because I CAN ¯\_(ツ)_/¯

--- one.php
+++ one.php
@@ -1,19 +1,22 @@
 <?php 
-	$page_title = "Twelve Games";
+	$page_title = "a partridge";
 ?>
 <?php include("includes/head.html");?>
 
 <body>
+<div class="page-wrap">
 
+	<?php include("includes/header.html");?>
 	<?php include("includes/navigation.html");?>
 
-
 		<div class="main-content center">
-			<h1>this is a test</h1>
+			<h1>a partridge</h1>
 			<p>this is some content</p>
+			<div class="game"></div>
 		</div>
 
 	<?php include("includes/footer.html");?>
+</div>
 
 </body>
 
