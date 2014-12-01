7bf7a54cf0f2a7a536f36216fe7b4973224c3a75
Savannah <savannahmillion@gmail.com>
2014-12-01 17:14:56 -0600

    messed with homepage style

--- index.php
+++ index.php
@@ -10,8 +10,9 @@
 
 
 		<div class="main-content center page-wrap">
-			<h1 class="homepage">twelve games<br/>of xmas</h1>
-			<p>Starting on December 14, 2014, each day we will post a new micro game (based on the Christmas carol) until December 25th.</p>
+			<h1 class="homepage">twelve games<br/><span class="emphasis">of</span> xmas</h1>
+			<p>Starting on December 14, 2014, each day we will post a new micro game (based on the Christmas carol) until December 25th.<br/>
+				Twelve tiny games to get you in that holiday spirit.</p>
 		</div>
 
 	<?php include("includes/footer.html");?>
