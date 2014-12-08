ca568c08a31e2ffcdf62530a133683bb9aeabfbc
Savannah <savannahmillion@gmail.com>
2014-12-08 10:14:28 -0600

    homepage style

--- index.php
+++ index.php
@@ -3,14 +3,15 @@
 ?>
 <?php include("includes/head.html");?>
 
-<body>
+<body class="background-pattern">
 
 	<?php include("includes/header.html");?>
 	<?php include("includes/navigation.html");?>
 
 
 		<div class="main-content center page-wrap">
-			<h1 class="homepage">twelve games<br/><span class="emphasis">of</span> xmas</h1>
+			<img class="banner-img" src="img/twelvegames-banner.png">
+			<!--<h1 class="homepage">twelve games<br/><span class="emphasis">of</span> xmas</h1>-->
 			<p>Starting on December 14, 2014, each day we will post a new micro game (based on the Christmas carol) until December 25th.<br/>
 				Twelve tiny games to get you in that holiday spirit.</p>
 		</div>
