4ed03d0ec46aa30bbf2b08e404d03b97625ba24f
Savannah <savannahmillion@gmail.com>
2014-12-11 17:25:49 -0600

    fixed php footer thing, fixed the media query that made the game window get bigger bw 800 and 1024px, added about-game paragraph, added pg id stuff to stylesheet

--- five.php
+++ five.php
@@ -18,6 +18,15 @@
 					loadGameIfDateIsValid();
 				</script>
 			</div>
+
+			<div class="about-game">
+				<h3>about this game</h3>
+				<p>
+					In A Partridge, use the spacebar and arrow keys to grab pears. Try to avoid breaking the ornaments. A Partridge also works on mobile. Steer the bird by tilting your phone, and tap to fly up.<br/>
+				</p>
+				<small>Released December 14th, 2014</small>
+			</div>
+			
 		</div>	
 
 	<?php include("includes/footer.html");?>
