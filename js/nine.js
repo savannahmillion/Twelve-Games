966c9ffded1bcbf2511e825dd578300b075c39b7
d-lask <dlaskey92@gmail.com>
2014-12-20 11:51:51 -0600

    initial commit of nine.js

---
+++ js/nine.js
@@ -0,0 +1,177 @@
+unlock = unlockDates[8];
+
+function mainGame() {
+    game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.CANVAS, 'game-canvas', { preload: preload, create: create, update: update, render: render});
+
+    var GAME_OVER = 0;
+    var NOT_PLAYING = 1;
+    var PLAYING = 2;
+    var state = NOT_PLAYING;
+
+    var win;
+    var lose;
+    var tween;
+
+    var canRestart = false;
+
+    var sfx_bells;
+    var sfx_thump;
+
+    var endSoundCount = 0;
+    var END_SOUND_MAX = 3;
+
+    var prevY;
+    var testPos = false;
+
+    function preload () {
+        this.load.image('nice', 'img/nice.png');
+        this.load.image('naughty', 'img/naughty.png');
+
+        this.load.image('background', 'img/nine/background.png');
+
+        this.load.atlasJSONHash('lady', 'img/nine/dancing-lady.png', 'img/nine/lady_anim.json');
+
+        this.load.audio('sfx_bells', 'sfx/bells.wav');
+        this.load.audio('sfx_thump', 'sfx/thump.wav');
+    }
+
+    function create () {
+        setupGameScaling();
+        updateSize();
+        
+        sfx_bells = game.add.audio('sfx_bells');
+        sfx_bells.addMarker('bells', 0.0, 1.0);
+
+        sfx_thump = game.add.audio('sfx_thump');
+        sfx_thump.addMarker('thump', 0.0, 1.0);
+
+        var background = game.add.sprite(game.world.centerX, game.world.centerY, 'background');
+        background.anchor.setTo(0.5, 0.5);
+
+        var onTouch = function(pointer) {
+            if(state == PLAYING)
+            {
+                
+            }
+
+            if(state == NOT_PLAYING)
+            {
+                state = PLAYING;
+                initGame();
+            }
+
+            if(state == GAME_OVER && canRestart)
+            {
+                canRestart = false;
+
+                if(lose.visible)
+                {
+                    tween = game.add.tween(lose).to( {y: -GAME_HEIGHT/2}, 500, Phaser.Easing.Quadratic.In, true);
+                    tween.onComplete.add(dropOutComplete, this);
+                }
+                else if(win.visible)
+                {
+                    tween = game.add.tween(win).to( {y: -GAME_HEIGHT/2}, 500, Phaser.Easing.Quadratic.In, true);
+                    tween.onComplete.add(dropOutComplete, this);   
+                }
+            }
+        }
+        
+        game.input.onDown.add(onTouch, this);
+
+        win = game.add.sprite(game.world.centerX, -GAME_HEIGHT/2, 'nice');
+        win.anchor.setTo(0.5, 0.5);
+        win.visible = false;
+        
+        lose = game.add.sprite(game.world.centerX, -GAME_HEIGHT/2, 'naughty');
+        lose.anchor.setTo(0.5, 0.5);
+        lose.visible = false;
+    }
+
+    function initGame(){ 
+        endSoundCount = 0;
+    }
+
+    function update() {
+
+    }
+
+    function playerWin(){
+        game.time.events.remove(spawnMaidEvent);
+        game.time.events.remove(spawnChickenEvent);
+
+        win.visible = true;
+
+        tween = game.add.tween(win).to( {y: GAME_HEIGHT/2}, 2000, Phaser.Easing.Bounce.Out, true);
+        tween.onComplete.add(dropInComplete, this);
+        tween.onUpdateCallback(tweenUpdate, this);
+    }
+
+    function playerLose(){
+        game.time.events.remove(spawnMaidEvent);
+        game.time.events.remove(spawnChickenEvent);
+
+        lose.visible = true;
+
+        tween = game.add.tween(lose).to( {y: GAME_HEIGHT/2}, 2000, Phaser.Easing.Bounce.Out, true);
+        tween.onComplete.add(dropInComplete, this);
+        tween.onUpdateCallback(tweenUpdate, this);
+    }
+
+    function dropInComplete(){
+        canRestart = true;
+    }
+
+    function dropOutComplete(){
+        state = NOT_PLAYING;
+
+        lose.visible = false;
+        win.visible = false;
+    }
+
+    function tweenUpdate(){
+        var sprite;
+        var playerWins = false;
+        if(win.visible)
+        {
+            sprite = win;
+            playerWins = true;
+        }
+        else if (lose.visible)
+            sprite = lose;
+        else
+            return;
+
+        if(sprite.visible){
+            if(testPos)
+            {
+                if(prevY > sprite.position.y)
+                {
+                    if(endSoundCount < END_SOUND_MAX)
+                    {
+                        if(playerWins)
+                            sfx_bells.play('bells', 0, 0.3);
+                        else
+                            sfx_thump.play('thump', 0, 0.5);
+
+                        endSoundCount++;
+                        testPos = false;
+                    }
+                }
+            }
+            else
+            {
+                if(endSoundCount < END_SOUND_MAX)
+                {
+                    if(prevY < sprite.position.y)
+                        testPos = true;
+                }
+            }
+            
+            prevY = sprite.position.y;
+        }
+    }
+
+    function render() {
+    }
+};
