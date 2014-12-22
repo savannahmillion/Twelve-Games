389d4fb0027ef3d8396449481ca3f571dc571426
David Laskey <dlaskey92@gmail.com>
2014-12-21 21:32:02 -0600

    Finished game nine

--- js/nine.js
+++ js/nine.js
@@ -23,6 +23,9 @@ function mainGame() {
     var prevY;
     var testPos = false;
 
+    var winCount = 0;
+    var loopEvents = [];
+
     var positions = [
         new Phaser.Point(250, 175),
         new Phaser.Point(350, 100),
@@ -35,18 +38,71 @@ function mainGame() {
         new Phaser.Point(400, 225)
     ];
 
+    var ladies = [];
+    var ladyIndex = 0;
+
+    var gameLoop;
+
+    function shuffleArray(toShuffle){
+        for(index = toShuffle.length - 1; index >= 0; index--)
+        {
+            var tempVal = toShuffle[index];
+            var tempIndex = game.rnd.integerInRange(0, toShuffle.length - 1);
+            toShuffle[index] = toShuffle[tempIndex];
+            toShuffle[tempIndex] = tempVal;
+        }
+    }
+
     function Lady(position){
+        this.highlight = game.add.sprite(position.x, position.y, 'circle');
+        this.highlight.anchor.setTo(0.5, 0.5);
+
+        this.highlight.visible = false;
+
         this.sprite = game.add.sprite(position.x, position.y, 'lady');
         this.sprite.anchor.setTo(0.5, 0.5);
 
         this.sprite.animations.add('dance', [0, 1, 2, 1]);
-        this.sprite.animations.add('cheer', [3]);
-        this.sprite.play('cheer', 10, true);
+        this.sprite.animations.add('surprise', [3]);
 
-        this.highlight = game.add.sprite(position.x, position.y, 'circle');
-        this.highlight.anchor.setTo(0.5, 0.5);
+        this.isDancing = false;
+
+        ladies.push(this);
     }
 
+    var dance = function(lady){
+        if(state == PLAYING){
+            var multiplier = ladies.indexOf(lady) + 1;
+
+            lady.highlight.scale.x -= 0.005 * multiplier/6;
+            lady.highlight.scale.y -= 0.005 * multiplier/6;
+            if(lady.highlight.scale.x <= 0.2)
+            {
+                playerLose();
+            }
+        }
+    }
+
+    var testLady = function(lady){
+        if(state == PLAYING){
+            if(this.highlight.visible && !this.isDancing)
+            {
+                game.time.events.remove(loopEvents[ladies.indexOf(this)]);
+                this.highlight.visible = false;
+                this.isDancing = true;
+
+                this.sprite.play('dance', 10, true);
+                winCount++;
+                if(winCount >= 9)
+                    playerWin();
+            }
+            else
+            {
+                this.sprite.play('surprise');
+                playerLose();
+            }
+        }
+    }
 
     function preload () {
         this.load.image('nice', 'img/nice.png');
@@ -75,15 +131,10 @@ function mainGame() {
         background.anchor.setTo(0.5, 0.5);
 
         var onTouch = function(pointer) {
-            if(state == PLAYING)
-            {
-                
-            }
-
             if(state == NOT_PLAYING)
             {
-                state = PLAYING;
                 initGame();
+                state = PLAYING;
             }
 
             if(state == GAME_OVER && canRestart)
@@ -100,10 +151,17 @@ function mainGame() {
                     tween = game.add.tween(win).to( {y: -GAME_HEIGHT/2}, 500, Phaser.Easing.Quadratic.In, true);
                     tween.onComplete.add(dropOutComplete, this);   
                 }
+
+                cleanup();
             }
         }
         
         game.input.onDown.add(onTouch, this);
+
+        for(i = 0; i < positions.length; i++)
+        {
+            new Lady(positions[i]);
+        }
 
         win = game.add.sprite(game.world.centerX, -GAME_HEIGHT/2, 'nice');
         win.anchor.setTo(0.5, 0.5);
@@ -112,15 +170,57 @@ function mainGame() {
         lose = game.add.sprite(game.world.centerX, -GAME_HEIGHT/2, 'naughty');
         lose.anchor.setTo(0.5, 0.5);
         lose.visible = false;
+    }
 
-        for(i = 0; i < positions.length; i++)
+    function StartLady(){
+        var lady = ladies[ladyIndex];
+        lady.highlight.scale = new Phaser.Point(1.0, 1.0);
+        lady.highlight.visible = true;
+
+        var danceEvent = game.time.events.loop(Phaser.Timer.SECOND / 60, dance, this, lady);
+        loopEvents.push(danceEvent);
+
+        ladyIndex++;
+
+        gameLoop.delay -= Phaser.Timer.SECOND * 0.2;
+    }
+
+    function cleanup(){
+        game.time.events.remove(gameLoop);
+
+        for(i = 0; i < loopEvents.length; i++)
         {
-            new Lady(positions[i]);
+            game.time.events.remove(loopEvents[i]);
         }
+
+        for(i = 0; i < ladies.length; i++)
+        {
+            ladies[i].highlight.visible = false;
+            ladies[i].sprite.animations.play('dance', 10, false);
+            ladies[i].sprite.animations.stop();
+            ladies[i].sprite.animations.currentAnim.frame = 0;
+            ladies[i].isDancing = false;
+            ladies[i].sprite.inputEnabled = false;
+        }
+
+        loopEvents = [];
     }
 
     function initGame(){ 
         endSoundCount = 0;
+        winCount = 0;
+        ladyIndex = 0;
+
+        shuffleArray(ladies);
+
+        for(i = 0; i < ladies.length; i++)
+        {
+            ladies[i].sprite.inputEnabled = true;
+            ladies[i].sprite.events.onInputDown.add(testLady, ladies[i]);
+        }
+
+        gameLoop = game.time.events.repeat(Phaser.Timer.SECOND * 1.8, 8, StartLady, game, ladyIndex);
+        StartLady();
     }
 
     function update() {
@@ -128,8 +228,7 @@ function mainGame() {
     }
 
     function playerWin(){
-        game.time.events.remove(spawnMaidEvent);
-        game.time.events.remove(spawnChickenEvent);
+        state = GAME_OVER;
 
         win.visible = true;
 
@@ -139,8 +238,7 @@ function mainGame() {
     }
 
     function playerLose(){
-        game.time.events.remove(spawnMaidEvent);
-        game.time.events.remove(spawnChickenEvent);
+        state = GAME_OVER;
 
         lose.visible = true;
 
--- nine.php
+++ nine.php
@@ -19,9 +19,12 @@
 			<div class="about-game" id="about-game">
 				<h3>about this game</h3>
 				<p>
-					In A Partridge, use the spacebar and arrow keys to grab pears. Try to avoid breaking the ornaments. A Partridge also works on mobile. Steer the bird by tilting your phone, and tap to fly up.<br/>
+					Goal: select the ladies who want to dance before their timer runs out! (blue circle disappears)<br/><br/>
+
+					Desktop: click to start and select.<br/>
+					Mobile: tap to start and select.<br/>	
 				</p>
-				<small>Released December 14th, 2014</small>
+				<small>Released December 22nd, 2014</small>
 			</div>
 
 		</div>	
